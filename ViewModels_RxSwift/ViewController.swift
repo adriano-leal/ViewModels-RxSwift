de3e6e4d6331ee8a96eaac191853ad94a2b96b5e
adriano <adriano.ramos@realizecfi.com.br>
2020-02-12 09:43:45 -0300

    Initial Commit

---
+++ ViewModels_RxSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewModels_RxSwift
+//
+//  Created by Adriano Ramos on 12/02/20.
+//  Copyright © 2020 Adriano Ramos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
